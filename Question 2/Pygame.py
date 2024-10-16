52c11249d8fae3be79367a9d86fb7cc4db0e0427
ReeceColgan <s377586@students.cdu.edu.au>
2024-10-16 10:40:40 +0800

    Update Pygame.py

--- Question 2/Pygame.py
+++ Question 2/Pygame.py
@@ -83,10 +83,10 @@ def draw_bg():
     screen.fill(BG)
     width = sky_img.get_width()
     for x in range (5):
-        screen.blit(sky_img ((x * width) - bg_scroll * 0.5, 0))
-        screen.blit(mountain_img((x * width) - bg_scroll * 0.6, SCREEN_HEIGHT - mountain_img.ger_height() - 300))
-        screen.blit(pine1_img((x * width) - bg_scroll * 0.7, SCREEN_HEIGHT - pine1_img.ger_height() - 150))
-        screen.blit(pine2_img((x * width) - bg_scroll * 0.8, SCREEN_HEIGHT - pine2_img.ger_height()))
+        screen.blit(sky_cloud_img ((x * width) - bg_scroll * 0.5, 0))
+        screen.blit(mountain_img((x * width) - bg_scroll * 0.6, SCREEN_HEIGHT - mountain_img.get_height() - 300))
+        screen.blit(pine1_img((x * width) - bg_scroll * 0.7, SCREEN_HEIGHT - pine1_img.get_height() - 150))
+        screen.blit(pine2_img((x * width) - bg_scroll * 0.8, SCREEN_HEIGHT - pine2_img.get_height()))
 class Soldier(pygame.sprite.Sprite):
     def __init__(self, char_type, x, y, scale, speed, ammo, grenades):
         pygame.sprite.Sprite.__init__(self)
